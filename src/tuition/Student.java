f788aaed7f5fc010e1e1893e61c962eb13648c1e
Stephen Koch <stephen.koch96@gmail.com>
2021-10-05 12:24:03 -0400

    Initial commit.

---
+++ src/tuition/Student.java
@@ -0,0 +1,18 @@
+package tuition;
+
+/*
+    @author sak5680
+*/
+public class Student {
+    private String name;
+    private String studentID;
+    private double tuition;
+    
+    public void setName(String n){
+        name = n;
+    }
+    
+    public void setID(String id){
+        studentID = id;
+    }
+}
